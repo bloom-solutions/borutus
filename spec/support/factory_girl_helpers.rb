e25752054a7c162f8d7ab70e1af2a071de9f6743
Ace Subido <ace.subido@gmail.com>
2019-10-24 02:15:03 +0800

    Rename factory_girl_helpers to factory_bot_helpers

--- spec/support/factory_girl_helpers.rb
+++
@@ -1,8 +0,0 @@
-module FactoryBotHelpers
-  def self.reload
-    FactoryBot.factories.clear()
-    FactoryBot.sequences.clear()
-    FactoryBot.traits.clear()
-    FactoryBot.find_definitions()
-  end
-end
