e05ce4240e987c9fefa5c1aec85a1951ffb4b2d6
Ace Subido <ace.subido@gmail.com>
2019-10-24 02:12:17 +0800

    Fix rubocop lint warnings in account_factory

--- spec/factories/account_factory.rb
+++ spec/factories/account_factory.rb
@@ -1,32 +1,32 @@
 FactoryBot.define do
-  factory :account, :class => Borutus::Account do |account|
+  factory :account, class: Borutus::Account do |account|
     account.name
-    account.contra false
+    account.contra { false }
   end
 
-  factory :asset, :class => Borutus::Asset do |account|
+  factory :asset, class: Borutus::Asset do |account|
     account.name
-    account.contra false
+    account.contra { false }
   end
 
-  factory :equity, :class => Borutus::Equity do |account|
+  factory :equity, class: Borutus::Equity do |account|
     account.name
-    account.contra false
+    account.contra { false }
   end
 
-  factory :expense, :class => Borutus::Expense do |account|
+  factory :expense, class: Borutus::Expense do |account|
     account.name
-    account.contra false
+    account.contra { false }
   end
 
-  factory :liability, :class => Borutus::Liability do |account|
+  factory :liability, class: Borutus::Liability do |account|
     account.name
-    account.contra false
+    account.contra { false }
   end
 
-  factory :revenue, :class => Borutus::Revenue do |account|
+  factory :revenue, class: Borutus::Revenue do |account|
     account.name
-    account.contra false
+    account.contra { false }
   end
 
   sequence :name do |n|
