113e579f0259a8921d703865eb2cbbc7f81cf013
Ace Subido <ace.subido@gmail.com>
2019-10-25 18:27:44 +0800

    Add reset counter logic when creating the counter_cache

--- db/migrate/20191025095830_add_borutus_amount_counter_cache.rb
+++ db/migrate/20191025095830_add_borutus_amount_counter_cache.rb
@@ -1,5 +1,15 @@
 class AddBorutusAmountCounterCache < ActiveRecord::Migration[4.2]
-  def change
+
+  def up
     add_column :borutus_accounts, :borutus_amounts_count, :integer
+
+    Borutus::Account.all.pluck(:id).each do |id|
+      Borutus::Account.reset_counters(id, :borutus_amounts)
+    end
   end
+
+  def down
+    remove_column :borutus_accounts, :borutus_amounts_count
+  end
+
 end
--- fixture_rails_root/db/migrate/20191025100256_add_borutus_amount_counter_cache.borutus.rb
+++ fixture_rails_root/db/migrate/20191025100256_add_borutus_amount_counter_cache.borutus.rb
@@ -1,6 +1,16 @@
 # This migration comes from borutus (originally 20191025095830)
 class AddBorutusAmountCounterCache < ActiveRecord::Migration[4.2]
-  def change
+
+  def up
     add_column :borutus_accounts, :borutus_amounts_count, :integer
+
+    Borutus::Account.all.pluck(:id).each do |id|
+      Borutus::Account.reset_counters(id, :borutus_amounts)
+    end
   end
+
+  def down
+    remove_column :borutus_accounts, :borutus_amounts_count
+  end
+
 end
