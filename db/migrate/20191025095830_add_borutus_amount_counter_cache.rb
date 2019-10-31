cec6508995215329c20c8a1db924432df85f2715
Ace Subido <ace.subido@gmail.com>
2019-10-31 12:41:26 +0800

    Remove broken counter cache migration

--- db/migrate/20191025095830_add_borutus_amount_counter_cache.rb
+++ db/migrate/20191025095830_add_borutus_amount_counter_cache.rb
@@ -2,11 +2,6 @@ class AddBorutusAmountCounterCache < ActiveRecord::Migration[4.2]
 
   def up
     add_column :borutus_accounts, :borutus_amounts_count, :integer
-
-    Borutus::Account.all.pluck(:id).each do |id|
-      Borutus::Account.reset_counters(id, :debit_amounts)
-      Borutus::Account.reset_counters(id, :credit_amounts)
-    end
   end
 
   def down
