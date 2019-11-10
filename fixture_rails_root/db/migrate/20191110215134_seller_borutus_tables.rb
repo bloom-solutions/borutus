77764d854131794b288172d0a7a0e81fc7c277d8
Ronald Walker <rondwalker22@gmail.com>
2019-11-10 14:53:04 -0800

    fix typo

--- fixture_rails_root/db/migrate/20191110215134_seller_borutus_tables.rb
+++ fixture_rails_root/db/migrate/20191110215134_seller_borutus_tables.rb
@@ -1,6 +1,6 @@
 class SellerBorutusTables < ActiveRecord::Migration[4.2]
   def change
-    # add a tenant column to borutus accounts table.
+    # add a seller column to borutus accounts table.
     add_column :borutus_accounts, :seller_id, :integer, index: true
   end
 end
